9b65d5a8d2fc64093ea012f3a01deb9cff383e8c
solidguy7 <kirill15grishkov01@gmail.com>
2023-01-22 21:04:45 +0300

    Add trigram similarity

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,7 +6,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 # from django.views.generic import ListView
 
 
@@ -91,10 +91,7 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body')
-            search_query = SearchQuery(query)
-            results = Post.published.annotate(search=search_vector,
-                                              rank=SearchRank(search_vector, search_query)
-                                              ).filter(search=search_query).order_by('-rank')
+            results = Post.published.annotate(similarity=TrigramSimilarity('title', query)
+                                              ).filter(similarity__gt=0.1).order_by('-similarity')
 
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
 
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'kirill15grishkov01@gmail.com'
-EMAIL_HOST_PASSWORD = 'pcvyoybbqpfzhlem'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
